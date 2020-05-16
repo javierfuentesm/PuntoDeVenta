23821deae6af6ab806a423a740326cee8a7fa9d5
Javier <javier.fuentesm@hotmail.com>
2020-05-16 12:45:40 -0500

    Cambios de iconos

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -46,7 +46,7 @@ const Home = () => {
 
   const _pickImage = async () => {
     try {
-      let result = await ImagePicker.launchImageLibraryAsync({
+      let result = await ImagePicker.launchCameraAsync({
         mediaTypes: ImagePicker.MediaTypeOptions.All,
         allowsEditing: true,
         aspect: [4, 3],
